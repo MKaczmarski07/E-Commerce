908cd8a879c31fc052e2108178509b7dbec1cd34
Mikołaj Kaczmarski <mikolajkaczmarski07@onet.pl>
2023-10-11 19:44:51 +0200

    Added Angular Fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,29 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { SliderComponent } from './home/slider/slider.component';
 import { ProductViewComponent } from './product-view/product-view.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environments';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MenuComponent, SliderComponent, ProductViewComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    SliderComponent,
+    ProductViewComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
