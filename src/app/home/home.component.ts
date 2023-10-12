e9f6ed281fbd2cde2589f1626a8aa9739fed9b77
Mikołaj Kaczmarski <mikolajkaczmarski07@onet.pl>
2023-10-12 09:51:01 +0200

    Added communication with database

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  gender: string;
+  name: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private firestore: Firestore = inject(Firestore);
+  shoes: Observable<Item[]> | undefined;
+
+  getCollections() {
+    const shoesCollection = collection(this.firestore, 'shoes') as any;
+    this.shoes = collectionData<Item>(shoesCollection);
+  }
+
+  ngOnInit() {
+    this.getCollections();
+  }
+}
--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.scss']
+  styleUrls: ['./product-view.component.scss'],
 })
 export class ProductViewComponent {
-
+  @Input() name = 'Air Jordan Low Retro';
+  @Input() price = 579.99;
+  @Input() gender = 'men';
+  @Input() type = 'Buty męskie';
 }
